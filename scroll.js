34625133356d6620efd489f7d64e33795824b5b7
johnvenkiah <contact@johnvenkiah.com>
2023-02-03 00:13:37 +0100

    add code for fade on scroll for subtitle in header!

--- scroll.js
+++ scroll.js
@@ -17,3 +17,34 @@ export function scrollToTop(element) {
 window.onload = () => {
   document.querySelector('body').style.opacity = 1;
 };
+
+var header = document.querySelector('.logo-p');
+
+function fadeOutOnScroll(element) {
+  var scrollLogo = document.querySelector('.top-element');
+
+  if (!element || !scrollLogo) {
+    return;
+  }
+
+  var distanceToTop =
+    window.pageYOffset + scrollLogo.getBoundingClientRect().top;
+  var elementHeight = scrollLogo.offsetHeight;
+  var scrollTop = document.documentElement.scrollTop;
+
+  var opacity = 1;
+
+  if (scrollTop > distanceToTop) {
+    opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+  }
+
+  if (opacity >= 0) {
+    element.style.opacity = opacity;
+  }
+}
+
+function scrollHandler() {
+  fadeOutOnScroll(header);
+}
+
+window.addEventListener('scroll', scrollHandler);
